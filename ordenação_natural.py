074ee41572a52ce5c04582097b2fc24dad7fe3e6
Analuvazz <analuvaz1997@gmail.com>
2024-09-02 22:32:32 -0300

    first commit

---
+++ ordenação_natural.py
@@ -0,0 +1,3 @@
+valor = [5,8,1,4]
+valor.sort()
+print(valor)
---
+++ ordenação_total.py
@@ -0,0 +1,5 @@
+atributo = ["Deliniador","Rimel","Base","Blush"]
+valor=[5,8,1,4]
+atributo.sort(key=len)
+valor.sort()
+print(atributo, valor)
